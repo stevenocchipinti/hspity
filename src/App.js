f39d1afac61683ed2e7913d3d5293d3317b0c3c4
Steve Occhipinti <dev@stevenocchipinti.com>
2018-04-28 02:10:17 +1000

    Basic header of the box component working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+import boxTitle from './boxTitle.png'
+import boxCornerLeft from './boxCornerLeft.png'
+import boxCornerRight from './boxCornerRight.png'
+import boxSpacerTop from './boxSpacerTop.png'
+import 'typeface-merriweather'
+
+const height = 86
+
+// const Box = styled.div`
+//   background-image: url(${parchment});
+// `
+
+const BoxTop = styled.div`
+  display: flex;
+`
+
+const LeftCorner = styled.div`
+  background-image: url(${boxCornerLeft});
+  height: ${height}px;
+  width: 42px;
+`
+
+const RightCorner = styled.div`
+  background-image: url(${boxCornerRight});
+  height: ${height}px;
+  width: 42px;
+`
+
+const Title = styled.div`
+  background-image: url(${boxTitle});
+  font-family: merriweather, serif;
+  font-size: 12px;
+  width: 231px;
+  height: ${height}px;
+  line-height: 40px;
+  color: #151515;
+  text-align: center;
+`
+
+const TopSpacer = styled.div`
+  background-image: url(${boxSpacerTop});
+  height: ${height}px;
+  flex-grow: 1;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>
+          <BoxTop>
+            <LeftCorner />
+            <TopSpacer />
+            <Title>Add a pack</Title>
+            <TopSpacer />
+            <RightCorner />
+          </BoxTop>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
