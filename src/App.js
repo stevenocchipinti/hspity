01bc7d49c801d9f32b591b3e265ae44ece778853
Steve Occhipinti <dev@stevenocchipinti.com>
2018-04-28 04:32:21 +1000

    Basic footer of the box component working
    Needs a refactor but the basic concept is working!
    Pretty fiddly to get the pixel sizes correct though :/

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, {Component} from 'react'
 import styled from 'styled-components'
 
-import boxTitle from './boxTitle.png'
 import boxCornerLeft from './boxCornerLeft.png'
-import boxCornerRight from './boxCornerRight.png'
 import boxSpacerTop from './boxSpacerTop.png'
-import boxSpacerRight from './boxSpacerRight.png'
-import boxSpacerLeft from './boxSpacerLeft.png'
+import boxTitle from './boxTitle.png'
+import boxCornerRight from './boxCornerRight.png'
 
+import boxSpacerLeft from './boxSpacerLeft.png'
 import velvet from './velvet.jpg'
+import boxSpacerRight from './boxSpacerRight.png'
+
+import boxCornerBottomLeft from './boxCornerBottomLeft.png'
+import boxSpacerBottom from './boxSpacerBottom.png'
+import boxCornerBottomRight from './boxCornerBottomRight.png'
 
 import 'typeface-merriweather'
 
-const height = 86
+const segmentHeight = 109
 
 const Box = styled.div`
   font-family: merriweather, serif;
+  line-height: 22px;
 `
 
 const BoxSegment = styled.div`
@@ -24,13 +29,25 @@ const BoxSegment = styled.div`
 
 const LeftCorner = styled.div`
   background-image: url(${boxCornerLeft});
-  height: ${height}px;
+  height: ${segmentHeight}px;
   width: 54px;
 `
 
 const RightCorner = styled.div`
   background-image: url(${boxCornerRight});
-  height: ${height}px;
+  height: ${segmentHeight}px;
+  width: 54px;
+`
+
+const LeftBottomCorner = styled.div`
+  background-image: url(${boxCornerBottomLeft});
+  height: ${segmentHeight}px;
+  width: 54px;
+`
+
+const RightBottomCorner = styled.div`
+  background-image: url(${boxCornerBottomRight});
+  height: ${segmentHeight}px;
   width: 54px;
 `
 
@@ -38,17 +55,24 @@ const Title = styled.div`
   background-image: url(${boxTitle});
   font-size: 12px;
   width: 231px;
-  height: ${height}px;
+  height: ${segmentHeight}px;
   line-height: 40px;
   color: #151515;
   text-align: center;
   z-index: 1;
 `
 
+const BottomSpacer = styled.div`
+  z-index: 1;
+  background-image: url(${boxSpacerBottom});
+  height: ${segmentHeight}px;
+  flex-grow: 1;
+`
+
 const TopSpacer = styled.div`
   z-index: 1;
   background-image: url(${boxSpacerTop});
-  height: ${height}px;
+  height: ${segmentHeight}px;
   flex-grow: 1;
 `
 
@@ -67,8 +91,9 @@ const RightSpacer = styled.div`
 const Content = styled.div`
   background-image: url(${velvet});
   flex-grow: 1;
-  padding: 40px 10px;
-  margin-top: -40px;
+  padding: 30px 15px 15px;
+  margin-top: -50px;
+  margin-bottom: -70px;
 `
 
 class App extends Component {
@@ -88,6 +113,12 @@ class App extends Component {
             <LeftSpacer />
             <Content>This is where the content goes</Content>
             <RightSpacer />
+          </BoxSegment>
+
+          <BoxSegment>
+            <LeftBottomCorner />
+            <BottomSpacer />
+            <RightBottomCorner />
           </BoxSegment>
         </Box>
       </div>
