dc5760ecddf018e895d5d69c0af71dd219b080ec
Steve Occhipinti <dev@stevenocchipinti.com>
2018-04-28 03:37:01 +1000

    Basic content of the box component working (middle)
    Using negative margins and z-index means the velvet background will always look
    like its lined up with the segment above or below it.
    
    Currently if the box becomes bigger than the size of the velvet image, it will
    have a seam where it repeats. This can probably be handled two ways:
    
    1. Prevent the box from getting that big with a max-width, max-height
    2. Use Gimp to make the velvet image seamless and repeatable
       (filters -> map -> make seamless)

--- src/App.js
+++ src/App.js
@@ -5,60 +5,91 @@ import boxTitle from './boxTitle.png'
 import boxCornerLeft from './boxCornerLeft.png'
 import boxCornerRight from './boxCornerRight.png'
 import boxSpacerTop from './boxSpacerTop.png'
+import boxSpacerRight from './boxSpacerRight.png'
+import boxSpacerLeft from './boxSpacerLeft.png'
+
+import velvet from './velvet.jpg'
+
 import 'typeface-merriweather'
 
 const height = 86
 
-// const Box = styled.div`
-//   background-image: url(${parchment});
-// `
+const Box = styled.div`
+  font-family: merriweather, serif;
+`
 
-const BoxTop = styled.div`
+const BoxSegment = styled.div`
   display: flex;
 `
 
 const LeftCorner = styled.div`
   background-image: url(${boxCornerLeft});
   height: ${height}px;
-  width: 42px;
+  width: 54px;
 `
 
 const RightCorner = styled.div`
   background-image: url(${boxCornerRight});
   height: ${height}px;
-  width: 42px;
+  width: 54px;
 `
 
 const Title = styled.div`
   background-image: url(${boxTitle});
-  font-family: merriweather, serif;
   font-size: 12px;
   width: 231px;
   height: ${height}px;
   line-height: 40px;
   color: #151515;
   text-align: center;
+  z-index: 1;
 `
 
 const TopSpacer = styled.div`
+  z-index: 1;
   background-image: url(${boxSpacerTop});
   height: ${height}px;
   flex-grow: 1;
+`
+
+const LeftSpacer = styled.div`
+  background-image: url(${boxSpacerLeft});
+  width: 54px;
+  flex-shrink: 0;
+`
+
+const RightSpacer = styled.div`
+  background-image: url(${boxSpacerRight});
+  width: 54px;
+  flex-shrink: 0;
+`
+
+const Content = styled.div`
+  background-image: url(${velvet});
+  flex-grow: 1;
+  padding: 40px 10px;
+  margin-top: -40px;
 `
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <BoxTop>
+        <Box>
+          <BoxSegment>
             <LeftCorner />
             <TopSpacer />
             <Title>Add a pack</Title>
             <TopSpacer />
             <RightCorner />
-          </BoxTop>
-        </div>
+          </BoxSegment>
+
+          <BoxSegment>
+            <LeftSpacer />
+            <Content>This is where the content goes</Content>
+            <RightSpacer />
+          </BoxSegment>
+        </Box>
       </div>
     )
   }
