1f9133f010aa3de0acabe1d1378ecc2da96cb9ed
Steve Occhipinti <dev@stevenocchipinti.com>
2018-05-03 11:12:54 +1000

    Make the ProgressBar have animated smoke!
    Just like the game. Technique copied from here:
    https://css-tricks.com/creating-a-css-sliding-background-effect

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -6,6 +6,7 @@ import headRare from './head-rare.png'
 import headEpic from './head-epic.png'
 import headLegendary from './head-legendary.png'
 import bar from './bar.png'
+import smoke from './bar-smoke.png'
 import indicator from './indicator.png'
 import empty from './bar-empty.png'
 import cap from './bar-cap.png'
@@ -29,13 +30,34 @@ const Head = styled.div`
   background-image: url(${props => headGems[props.rarity]});
   width: 66px;
   flex-shrink: 0;
-  z-index: 1;
+  z-index: 2;
 `
 
 const FullBar = styled.div`
+  position: relative;
   background-image: url(${bar});
   flex-grow: ${props => props.percentage};
   margin-left: -3px;
+  overflow: hidden;
+`
+const Smoke = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: url(${smoke});
+  height: 560px;
+  width: 5076px;
+  animation: slide 60s linear infinite;
+  @keyframes slide {
+    0% {
+      transform: translate3d(-1692px, 0, 0);
+    }
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+  }
 `
 
 const Indicator = styled.div`
@@ -43,6 +65,7 @@ const Indicator = styled.div`
   width: 13px;
   display: ${props => (props.visible ? 'inherit' : 'none')};
   margin-left: -13px;
+  z-index: 1;
 `
 const EmptyBar = styled.div`
   background-image: url(${empty});
@@ -70,7 +93,9 @@ const ProgressBar = ({rarity, numerator, denominator}) => {
   return (
     <Wrapper>
       <Head rarity={rarity} />
-      <FullBar percentage={percentage} />
+      <FullBar percentage={percentage}>
+        <Smoke />
+      </FullBar>
       <Indicator visible={percentage < 1} />
       <EmptyBar percentage={1 - percentage} />
       <Cap />
