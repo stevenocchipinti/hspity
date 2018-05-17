952e1d7d19f4a934867baa86f9644deef30e8fd7
Steve Occhipinti <dev@stevenocchipinti.com>
2018-05-17 21:43:05 +1000

    Add an overlay to draw attention

--- src/components/Foo/index.js
+++ src/components/Foo/index.js
@@ -8,7 +8,22 @@ import ProgressBar from '../ProgressBar'
 
 const FlexProgressBar = styled(ProgressBar)`
   width: 300px;
-}
+`
+
+const Wrapper = styled.div`
+  height: '100vh';
+  display: 'flex';
+  justify-content: 'center';
+  align-items: 'center';
+`
+
+const Overlay = styled.div`
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  left: 0;
 `
 
 class Bar extends Component {
@@ -24,10 +39,14 @@ class Bar extends Component {
         ...this.hiddenStyle,
         ease: Elastic.easeOut.config(1, 0.5),
       })
+      .from('.overlay', 1, {autoAlpha: 0}, 0)
+
       .addLabel('bar')
-      .add(() => this.setState({progress: 30}), 'bar') // This takes 1 second via CSS
+      // This takes 1 second via CSS
+      .add(() => this.setState({progress: 30}), 'bar')
       .to('.bar', 0.2, {transform: 'scale(1.1)', ease: Power4.easeIn}, 'bar')
       .to('.bar', 0.8, {transform: 'scale(1)', ease: Power4.easeOut})
+
       .add(() => this.props.onComplete(), '+=0.5')
     callback()
   }
@@ -38,16 +57,20 @@ class Bar extends Component {
       ease: Back.easeIn.config(2),
       onComplete: callback,
     })
+    TweenLite.to('.overlay', 1, {autoAlpha: 0}, 0)
   }
 
   render() {
     return (
-      <FlexProgressBar
-        rarity="legendary"
-        numerator={this.state.progress}
-        denominator={40}
-        className="bar"
-      />
+      <Wrapper>
+        <Overlay className="overlay" />
+        <FlexProgressBar
+          rarity="legendary"
+          numerator={this.state.progress}
+          denominator={40}
+          className="bar"
+        />
+      </Wrapper>
     )
   }
 }
